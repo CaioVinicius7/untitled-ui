f6e427e2b5d2275fb195b9227f406a684ad32011
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-01 20:26:56 -0300

    feat: :tada: Base do projeto e configurações iniciais

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid h-screen place-items-center bg-slate-900 text-slate-100">
+      <h1 className="text-5xl font-bold">Hello, tailwind!</h1>
+    </div>
+  );
+}
