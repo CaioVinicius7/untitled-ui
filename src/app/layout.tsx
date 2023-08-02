1539be3826f56492641f8e09774f43aff650ff58
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-01 21:55:36 -0300

    feat: :lipstick: Estrutura do Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid min-h-screen grid-cols-app">
+          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+
+          <main className="px-4 pb-12 pt-8">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="grid h-screen place-items-center bg-slate-900 text-slate-100">
-      <h1 className="text-5xl font-bold">Hello, tailwind!</h1>
-    </div>
-  );
+  return <h1>Hello, tailwind!</h1>;
 }
