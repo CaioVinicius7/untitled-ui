0c3cc2f2056c3abc648e0a41e5af5d5073c9862f
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-03 10:55:57 -0300

    feat: :lipstick: Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 
+import { Sidebar } from "@root/components/Sidebar";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="pt-BR">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
-          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+          <Sidebar />
 
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export function Sidebar() {
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+    </aside>
+  );
+}
