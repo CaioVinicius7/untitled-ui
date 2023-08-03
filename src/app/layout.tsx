0f2bc6476c80beac3cf68dabab7b6050ffa55499
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-03 12:19:42 -0300

    chore: :mag: Alteração do favicon e title da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tailwind + Next",
+  title: "Untitled UI",
   description: "Tailwind + Next"
 };
 
