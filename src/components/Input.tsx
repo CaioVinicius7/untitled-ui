1c7c500e5b5229d1709726ef9e02acd988731a2c
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-05 19:33:15 -0300

    feat: :lipstick: Componente Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from "react";
+
+interface InputPrefixProps extends ComponentProps<"div"> {}
+
+function InputPrefix({ ...props }: InputPrefixProps) {
+  return <div {...props} />;
+}
+
+interface InputControlProps extends ComponentProps<"input"> {}
+
+function InputControl({ ...props }: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      {...props}
+    />
+  );
+}
+
+interface InputRootProps extends ComponentProps<"div"> {}
+
+function InputRoot({ ...props }: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3 shadow-sm focus-within:border-zinc-900"
+      {...props}
+    ></div>
+  );
+}
+
+export const Input = {
+  Root: InputRoot,
+  Prefix: InputPrefix,
+  Control: InputControl
+};
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import { Cog, LifeBuoy, Search } from "lucide-react";
 
+import { Input } from "../Input";
+
 import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
 import { NavItem } from "./MainNavigation/NavItem";
@@ -11,15 +13,13 @@ export function Sidebar() {
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-3 shadow-sm focus-within:border-zinc-900">
-        <Search className="h-5 w-5 text-zinc-500" />
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
 
-        <input
-          type="text"
-          placeholder="Search"
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
-        />
-      </div>
+        <Input.Control type="text" placeholder="Search" />
+      </Input.Root>
 
       <MainNavigation />
 
