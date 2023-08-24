f0c053ca09178350f1f1aecf5f40274da446f82a
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-24 03:30:36 -0300

    feat: :lipstick: Tema dark do formulário

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 import { ChevronDown } from "lucide-react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 
@@ -12,7 +13,13 @@ interface SelectProps extends SelectPrimitive.SelectProps {
 export function Select({ placeholder, children, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600">
+      <SelectPrimitive.Trigger
+        className={twMerge([
+          "flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none",
+          "focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600",
+          "dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-400 dark:focus:border-violet-500 dark:focus:ring-violet-500/10 dark:data-[placeholder]:text-zinc-400"
+        ])}
+      >
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
@@ -27,7 +34,7 @@ export function Select({ placeholder, children, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] animate-slideDownAndFade overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="z-10 w-[--radix-select-trigger-width] animate-slideDownAndFade overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm dark:border-zinc-700 dark:bg-zinc-800"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
