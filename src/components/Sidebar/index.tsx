684032b189e019bd51f9111277db144393ea390f
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-23 22:36:27 -0300

    feat: :iphone: Abertura da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,7 @@
-import { Cog, LifeBuoy, Search } from "lucide-react";
+"use client";
+
+import { Cog, LifeBuoy, Menu, Search } from "lucide-react";
+import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { Input } from "../Form/Input";
 
@@ -7,44 +10,58 @@ import { MainNavigation } from "./MainNavigation";
 import { NavItem } from "./MainNavigation/NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import { Button } from "../Button";
 
 export function Sidebar() {
   return (
-    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen">
+      <div className="flex items-center justify-between">
+        <Logo />
 
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-
-        <Input.Control type="text" placeholder="Search" />
-      </Input.Root>
-
-      <MainNavigation />
-
-      <div className="mt-auto flex flex-col gap-6">
-        <nav className="space-y-0.5">
-          <NavItem
-            href="#"
-            title="Support"
-            icon={LifeBuoy}
-            withExpandIndicator={false}
-          />
-          <NavItem
-            href="#"
-            title="Settings"
-            icon={Cog}
-            withExpandIndicator={false}
-          />
-        </nav>
-
-        <UsedSpaceWidget />
-
-        <div className="h-px bg-zinc-200" />
-
-        <Profile />
+        <Collapsible.Trigger className="lg:hidden" asChild>
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
       </div>
-    </aside>
+
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </Input.Prefix>
+
+          <Input.Control type="text" placeholder="Search" />
+        </Input.Root>
+
+        <MainNavigation />
+
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem
+              href="#"
+              title="Support"
+              icon={LifeBuoy}
+              withExpandIndicator={false}
+            />
+            <NavItem
+              href="#"
+              title="Settings"
+              icon={Cog}
+              withExpandIndicator={false}
+            />
+          </nav>
+
+          <UsedSpaceWidget />
+
+          <div className="h-px bg-zinc-200" />
+
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
